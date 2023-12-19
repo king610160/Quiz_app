1d9180ea765bba1659a7c01c2d87116aa46b1777
king610160 <king610160@gmail.com>
2023-12-19 18:21:08 +0800

    feat: add authenticated to the quiz route.

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,6 +2,10 @@ const { getUser, ensureAuthticated } = require('../helper/auth-helper')
 
 const authenticated = (req, res, next) => {
     if (ensureAuthticated(req)) next()
+    else {
+        req.flash('error_msg','You have no authenticated to enter this route.')
+        res.redirect('/users/login')
+    }
 }
 
 const authenticatedAdmin = (req, res, next) => {
--- routes/pages/index.js
+++ routes/pages/index.js
@@ -3,12 +3,16 @@ const router = express.Router()
 
 const users = require('./modules/users')
 const quiz = require('./modules/quiz')
+
+const { authenticated } = require('../../middleware/auth')
+
 // big bracket means to load function
 const { generalErrorHandler } = require('../../middleware/error-handler')
 
-router.use('/quiz', quiz)
+router.use('/quiz', authenticated,  quiz)
 router.use('/users', users)
 
+router.use('/', (req, res) => res.redirect('/users/login'))
 router.use('/', generalErrorHandler)  // use function
 
 module.exports = router
