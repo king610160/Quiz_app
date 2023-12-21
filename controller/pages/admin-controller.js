47776b9c0f7e348597a7d31852dfd5ea3785f01d
king610160 <king610160@gmail.com>
2023-12-21 13:13:27 +0800

    feat: Done with admin get all user info, and finish the router and display.

--- controller/pages/admin-controller.js
+++ controller/pages/admin-controller.js
@@ -1,6 +1,20 @@
+const { User } = require('../../models')
+
 const adminController = {
     home: (req, res) => {
         res.render('admin/home')
+    },
+    getAllUsers: async (req, res) => {
+        const users = await User.findAll({
+            raw: true,
+            nest: true
+        })
+        const setting = {
+            user: true
+        }
+        // console.log(users)
+        res.render('admin/allUser', {users, setting})
+        // res.redirect('/admin')
     }
 }
 
--- routes/pages/modules/admin.js
+++ routes/pages/modules/admin.js
@@ -4,5 +4,6 @@ const router = express.Router()
 const adminController = require('../../../controller/pages/admin-controller')
 
 router.get('/', adminController.home)
+router.get('/user', adminController.getAllUsers)
 
 module.exports = router
