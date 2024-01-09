2d83fc1dce0e4dd0abe002858a1bc33ce061a2e3
king610160 <king610160@gmail.com>
2024-01-09 14:37:18 +0800

    feat: fix host to 127.0.0.1.

--- config/config.js
+++ config/config.js
@@ -5,21 +5,21 @@ module.exports = {
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
+        host: '127.0.0.1',
         dialect: process.env.DB_DRIVER
     },
     test: {
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
+        host: '127.0.0.1',
         dialect: process.env.DB_DRIVER
     },
     production: {
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
+        host: '127.0.0.1',
         dialect: process.env.DB_DRIVER
     }
 }
