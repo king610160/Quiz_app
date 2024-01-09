ad104328217b49ae3ba1bbfed866cc6bb8981c93
king610160 <king610160@gmail.com>
2024-01-09 15:52:20 +0800

    feat: fix dialect to postgres.

--- config/config.js
+++ config/config.js
@@ -6,20 +6,20 @@ module.exports = {
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
-        dialect: process.env.DB_DRIVER
+        dialect: 'postgres'
     },
     test: {
         username: process.env.DB_USER_TEST,
         password: process.env.DB_PASSWORD_TEST,
         database: process.env.DB_NAME_TEST,
         host: process.env.DB_HOST_TEST,
-        dialect: process.env.DB_DRIVER_TEST
+        dialect: 'postgres'
     },
     production: {
         username: process.env.DB_USER_PRO,
         password: process.env.DB_PASSWORD_PRO,
         database: process.env.DB_NAME_PRO,
         host: process.env.DB_HOST_PRO,
-        dialect: process.env.DB_DRIVER_PRO
+        dialect: 'postgres'
     }
 }
