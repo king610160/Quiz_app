2734e9f45ed73efa166b57d07a381cc7c966fda3
king610160 <king610160@gmail.com>
2024-01-09 16:45:24 +0800

    feat: fix config.js's setting.

--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@ require('dotenv').config()
 
 module.exports = {
     development: {
-        username: process.env.DB_USER_DEV,
-        password: process.env.DB_PASSWORD_DEV,
-        database: process.env.DB_NAME_DEV,
-        host: process.env.DB_HOST_DEV,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
         dialect: 'postgres',
         port:'5432'
     },
