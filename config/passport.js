14c066649e21758c24a6f006d910148b8c7fb0cc
king610160 <king610160@gmail.com>
2024-01-16 09:29:57 +0800

    feat: fix callbackURL with environment parameter.

--- config/passport.js
+++ config/passport.js
@@ -37,7 +37,7 @@ passport.use(new LocalStrategy(
 passport.use(new FacebookStrategy({
     clientID: process.env.FB_PROJECT_NO,
     clientSecret: process.env.FB_PROJECT_PASSWORD,
-    callbackURL: 'http://localhost:3000/auth/facebook/callback',
+    callbackURL: `${process.env.WEB_URL}/auth/google/callback`,
     profileFields: ['email', 'displayName']
 }, async (accessToken, refreshToken, profile, done) =>  {
     try {
@@ -61,7 +61,7 @@ passport.use(new FacebookStrategy({
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_PROJECT_NO,
     clientSecret: process.env.GOOGLE_PROJECT_PASSWORD,
-    callbackURL: 'http://localhost:3000/auth/google/callback',
+    callbackURL: `${process.env.WEB_URL}/auth/google/callback`,
     profileFields: ['email', 'displayName']
 }, async (accessToken, refreshToken, profile, done) =>  {
     try {
