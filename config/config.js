ebc2bb9fcee55b52de45a8c7dfc429a97ace2e40
king610160 <king610160@gmail.com>
2024-03-01 15:11:21 +0800

    Fix the info to fit local.

--- config/config.js
+++ config/config.js
@@ -9,19 +9,5 @@ module.exports = {
         host: process.env.DB_HOST,
         dialect: 'postgres',
         port:'5432'
-    },
-    test: {
-        username: process.env.DB_USER_TEST,
-        password: process.env.DB_PASSWORD_TEST,
-        database: process.env.DB_NAME_TEST,
-        host: process.env.DB_HOST_TEST,
-        dialect: 'postgres'
-    },
-    production: {
-        username: process.env.DB_USER_PRO,
-        password: process.env.DB_PASSWORD_PRO,
-        database: process.env.DB_NAME_PRO,
-        host: process.env.DB_HOST_PRO,
-        dialect: 'postgres'
     }
 }
--- db/redis.js
+++ db/redis.js
@@ -2,7 +2,8 @@
 const redis = require('redis')
 
 const client = redis.createClient({
-    url: process.env.REDIS_URL
+    host: '127.0.0.1',
+    port: '6379'
 })
 
 module.exports = client
