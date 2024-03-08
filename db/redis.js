273ea9ae043c50c3bf15b4109b148ad438a0e1ea
king610160 <king610160@gmail.com>
2024-03-08 10:57:31 +0800

    icon fail, change icon style

--- db/redis.js
+++ db/redis.js
@@ -2,9 +2,9 @@
 const redis = require('redis')
 
 const client = redis.createClient({
-    url: process.env.REDIS_URL
-    // host: '127.0.0.1',
-    // port: '6379'
+    // url: process.env.REDIS_URL
+    host: '127.0.0.1',
+    port: '6379'
 })
 
 module.exports = client
